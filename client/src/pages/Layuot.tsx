48e43ed0258919fc145157754fe555157af4c9d2
Alexander-Verbin <sash.verbin@yandex.ru>
2023-10-12 19:28:43 +0300

    10 lesson

---
+++ client/src/pages/Layuot.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Layuot: FC = () => {
+	return (
+		<>
+			<div>Layuot</div>
+		</>
+	);
+};
---
+++ client/src/pages/Transactions.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Transactions: FC = () => {
+	return (
+		<>
+			<div>Transactions</div>
+		</>
+	);
+};
